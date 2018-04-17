bdf571fa2fe3f94c6f35fb593af9ea32423335b7
Hong.liu <hong.liu@mainiway.com>
2018-04-17 13:04:38 +0800

    feat(rebuild)

--- packages/routing/__tests__/http.test.js
+++ packages/routing/__tests__/http.test.js
@@ -2,7 +2,7 @@ import Koa from 'koa'
 import request from 'supertest'
 import http from 'http'
 
-import {DataType} from 'validator-decorators'
+import {DataType} from '@koaspring/validator'
 
 import {routing, Params, routingMiddleware} from '../'
 
